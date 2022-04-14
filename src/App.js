58a9b4f5c6ee68e5f42d99f8ebab42566c83b8be
Aibarys03 <aibarys0490@gmail.com>
2022-04-14 21:53:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+let App = () => {
+  return(
+    <div>
+      <header>
+<div className='text'>
+English Language Tutors
+</div>
+<div className='logo'>
+<img className='logol' src="../images/Tutor.png" alt="logo"/>
+</div>
+</header>
+<main>
+<div className='Standart-shapka'>
+<ul>
+    <li>Standart</li>
+    <li>35 000тг</li>
+  </ul>
+</div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className='Standart-footer'>
+  <ul>
+    <li>Group form of training</li>
+    <li>An experienced teacher</li>
+    <li>12 hours per month</li>
+    <li>The duration of the lesson is 60 minutes</li>
+  </ul>
+</div>
+<div className='button-first'><button className='button-first-but'>sign up</button></div>
+
+<div className='Silver-shapka'>
+<ul>
+    <li>Advanced</li>
+    <li>50 000тг</li>
+  </ul>
+</div>
+<div className='Silver-footer'>
+  <ul>
+    <li>Individual form of training</li>
+    <li>Foreign teacher</li>
+    <li>12 hours per month</li>
+    <li>The duration of the lesson is 60 minutes</li>
+  </ul>
+</div>
+<div className='button-second'><button className='button-second-but'>sign up</button></div>
+
+
+<div className='Professional-shapka'>
+<ul>
+    <li>Professional</li>
+    <li>80 000тг</li>
+  </ul>
+</div>
+<div className='Professional-footer'>
+  <ul>
+    <li>Individual form of training</li>
+    <li>Foreign teacher</li>
+    <li>16 hours per month</li>
+    <li>The duration of the lesson is 80 minutes</li>
+  </ul>
+</div>
+<div className='button-third'><button className='button-third-but'>sign up</button></div>
+</main>
+<footer>
+<div className='footer'>
+<div className='contacts'>Contacts:</div>
+<div className='number'>+77016677853</div>
+<div className='address'>Address: Almaty city,Auezov st. 65</div>
+</div>
+
+</footer>
+
+
+
+
+</div>
   );
 }
 
 export default App;
+
+
